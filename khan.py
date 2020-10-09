cded1495e088a71e479def67739bd8f3ff0af115
Badamala302 <72606242+Badamala302@users.noreply.github.com>
2020-10-09 16:41:06 +0500

    Update and rename Bilalo to khan.py

--- khan.py
+++ khan.py
@@ -115,8 +115,8 @@ print  """\033[1;91m
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "BILALO
-CorrectPassword = "BILALO
+CorrectUsername = "BOSS
+CorrectPassword = "BOSS
 
 loop = 'true'
 while (loop == 'true'):
